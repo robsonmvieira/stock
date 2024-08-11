4f6d3f3b82bc1df6d8ee3debe1bb09b8a666195d
Robson Maia <robsonmvieira@gmail.com>
2024-08-11 17:46:04 +0000

    feat(business): :sparkles: resolver session dependency injection

--- src/modules/session/session.module.ts
+++ src/modules/session/session.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common'
 import { EncryptRepository } from './infra/repository/encrypt.repository'
+import { SessionController } from './application/controllers/session.controller'
+import { LoginUseCase } from './application/usecases/login/login.usecase'
+import { IEncryptPort } from './domain/repositories/encrypt.port'
 
 @Module({
+  controllers: [SessionController],
   providers: [
     {
       provide: 'IEncryptPort',
       useClass: EncryptRepository
+    },
+
+    {
+      provide: LoginUseCase,
+      useFactory: (encryptPort: IEncryptPort) => new LoginUseCase(encryptPort),
+      inject: ['IEncryptPort']
     }
   ]
 })
