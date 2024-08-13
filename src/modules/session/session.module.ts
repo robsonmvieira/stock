cb0867e892a555c7a5214a07f387175f8a0650f2
Robson Maia <robsonmvieira@gmail.com>
2024-08-13 19:59:59 +0000

    feat(business): :sparkles: create jwt service

--- src/modules/session/session.module.ts
+++ src/modules/session/session.module.ts
@@ -3,19 +3,24 @@ import { EncryptRepository } from './infra/repository/encrypt.repository'
 import { SessionController } from './application/controllers/session.controller'
 import { LoginUseCase } from './application/usecases/login/login.usecase'
 import { IEncryptPort } from './domain/repositories/encrypt.port'
+import { ConfigModule } from '@modules/config/config.module'
+import { JwtService } from './application/services/jwt.service'
 
 @Module({
+  imports: [ConfigModule],
   controllers: [SessionController],
   providers: [
     {
       provide: 'IEncryptPort',
       useClass: EncryptRepository
     },
+    JwtService,
 
     {
       provide: LoginUseCase,
-      useFactory: (encryptPort: IEncryptPort) => new LoginUseCase(encryptPort),
-      inject: ['IEncryptPort']
+      useFactory: (JwtService: JwtService, encryptPort: IEncryptPort) =>
+        new LoginUseCase(encryptPort, JwtService),
+      inject: [JwtService, 'IEncryptPort']
     }
   ]
 })
