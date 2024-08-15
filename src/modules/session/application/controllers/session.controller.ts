ed19e0a87f49b290ccd2ef58a8868e9889c87ad4
Robson Maia <robsonmvieira@gmail.com>
2024-08-15 14:39:49 +0000

    feat(business): :recycle: update session controller

--- src/modules/session/application/controllers/session.controller.ts
+++ src/modules/session/application/controllers/session.controller.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common'
+import { Body, Controller, Inject, Post, Res } from '@nestjs/common'
 import { LoginDto } from '../usecases/login/dto/login.dto'
 import { LoginUseCase } from '../usecases/login/login.usecase'
-
+import { Response } from 'express'
+import { Public } from '@modules/shared/is-public-decorator'
 @Controller('login')
 export class SessionController {
   @Inject() private loginUseCase: LoginUseCase
 
+  @Public()
   @Post()
-  async login(@Body() dto: LoginDto): Promise<string> {
-    console.log('controller', dto)
-    this.loginUseCase.execute(dto)
-    return 'login'
+  async login(@Res() response: Response, @Body() dto: LoginDto) {
+    const data = await this.loginUseCase.execute(dto, response)
+    if (data.hasError) {
+      return response.status(400).json(data)
+    }
+    return response.status(200).json(data)
   }
 }
