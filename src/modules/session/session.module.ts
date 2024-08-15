2e353b71ccf34b54a818f4bb17f66e86e81032eb
Robson Maia <robsonmvieira@gmail.com>
2024-08-15 14:40:42 +0000

    refactor(business): :truck: split some class to specific new module

--- src/modules/session/session.module.ts
+++ src/modules/session/session.module.ts
@@ -1,27 +1,29 @@
 import { Module } from '@nestjs/common'
-import { EncryptRepository } from './infra/repository/encrypt.repository'
 import { SessionController } from './application/controllers/session.controller'
 import { LoginUseCase } from './application/usecases/login/login.usecase'
-import { IEncryptPort } from './domain/repositories/encrypt.port'
 import { ConfigModule } from '@modules/config/config.module'
 import { JwtService } from './application/services/jwt.service'
+import { EmployeeModule } from '@modules/employee/employee.module'
+import { IEmployeeRepository } from '@modules/employee/domain/repositories'
+import { IEncryptPort } from '@modules/encrypt/domain/repositories/encrypt.port'
+import { EncryptModule } from '@modules/encrypt/encrypt.module'
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, EmployeeModule, EncryptModule],
   controllers: [SessionController],
   providers: [
-    {
-      provide: 'IEncryptPort',
-      useClass: EncryptRepository
-    },
     JwtService,
 
     {
       provide: LoginUseCase,
-      useFactory: (JwtService: JwtService, encryptPort: IEncryptPort) =>
-        new LoginUseCase(encryptPort, JwtService),
-      inject: [JwtService, 'IEncryptPort']
+      useFactory: (
+        JwtService: JwtService,
+        encryptPort: IEncryptPort,
+        employeeRepository: IEmployeeRepository
+      ) => new LoginUseCase(encryptPort, JwtService, employeeRepository),
+      inject: [JwtService, 'IEncryptPort', 'IEmployeeRepository']
     }
-  ]
+  ],
+  exports: [JwtService]
 })
 export class SessionModule {}
