7787f9324d2197f567f38ea61ef27ffab5e7a172
Robson Maia <robsonmvieira@gmail.com>
2024-08-15 14:41:26 +0000

    feat(business): :sparkles: add new custom method to employee repository contract

--- src/modules/employee/domain/repositories/employee.port.ts
+++ src/modules/employee/domain/repositories/employee.port.ts
@@ -1,4 +1,8 @@
 import { IRepository } from 'src/modules/core/domain/repositories'
 import { EmployeeModel } from '../models'
 
-export interface IEmployeeRepository extends IRepository<EmployeeModel> {}
+export interface IEmployeeRepository extends IRepository<EmployeeModel> {
+  findByEmail(email: string): Promise<EmployeeModel>
+
+  findByDocument(document: string): Promise<EmployeeModel>
+}
