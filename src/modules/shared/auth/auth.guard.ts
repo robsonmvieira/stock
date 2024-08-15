ff2a0f965a7f326dbc873c23b11c398182ccae9f
Robson Maia <robsonmvieira@gmail.com>
2024-08-15 14:23:14 +0000

    feat(auth): :sparkles: add auth guard

---
+++ src/modules/shared/auth/auth.guard.ts
@@ -0,0 +1,32 @@
+import { JwtService } from '@modules/session/application/services/jwt.service'
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
+
+import { Reflector } from '@nestjs/core'
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest()
+    const token = request.cookies['Authorization']
+
+    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+      context.getHandler(),
+      context.getClass()
+    ])
+    if (isPublic) {
+      return true
+    }
+
+    if (!token) return false
+    try {
+      const payload = await this.jwtService.verifyAsync(token)
+      request.user = payload
+      return true
+    } catch (e) {
+      return false
+    }
+  }
+}
