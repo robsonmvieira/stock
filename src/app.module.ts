679c1a5ccc894d4334d5d5599c591e7398e3142a
Robson Maia <robsonmvieira@gmail.com>
2024-08-15 14:08:47 +0000

    feat(core): :sparkles: add new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,27 @@ import { ConfigModule } from './modules/config/config.module'
 import { DatabaseModule } from './modules/database/database.module'
 import { CacheModule } from './cache/cache.module'
 import { SessionModule } from './modules/session/session.module'
+import { EncryptModule } from './modules/encrypt/encrypt.module'
+import { SharedModule } from './modules/shared/shared.module'
+import { APP_GUARD } from '@nestjs/core'
+import { AuthGuard } from '@modules/shared/auth/auth.guard'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
+    SharedModule,
     CacheModule,
     EmployeeModule,
-    SessionModule
+    SessionModule,
+    EncryptModule
   ],
   controllers: [],
-  providers: []
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
+  ]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const config = new DocumentBuilder()
     .setTitle('API Docs')
     .setDescription('Endpoint List')
+    .addCookieAuth('Authorization', {
+      type: 'apiKey',
+      in: 'cookie',
+      name: 'Authorization',
+      description: 'Cookie with the auth token'
+    })
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('docs', app, document)
+  app.use(cookieParser())
+
   await app.listen(3000)
 }
 bootstrap()
