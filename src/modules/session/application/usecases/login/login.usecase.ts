608422b41fd7a14a5404caa5896d5cc5069203de
Robson Maia <robsonmvieira@gmail.com>
2024-08-15 14:35:07 +0000

    feat(auth): :passport_control: update login usecase

--- src/modules/session/application/usecases/login/login.usecase.ts
+++ src/modules/session/application/usecases/login/login.usecase.ts
@@ -1,16 +1,58 @@
 import { Injectable } from '@nestjs/common'
 import { LoginDto } from './dto/login.dto'
-import { IEncryptPort } from '@modules/session/domain/repositories/encrypt.port'
-
+import { JwtService } from '../../services/jwt.service'
+import { IEmployeeRepository } from '@modules/employee/domain/repositories'
+import { ModelOutput } from '@modules/core/application/usecases/common/model.output'
+import { IEncryptPort } from '@modules/encrypt/domain/repositories/encrypt.port'
+import { Response } from 'express'
 @Injectable()
 export class LoginUseCase {
-  constructor(private hashRepository: IEncryptPort) {}
+  constructor(
+    private hashRepository: IEncryptPort,
+    private jwtService: JwtService,
+    private employeeRepository: IEmployeeRepository
+  ) {}
 
-  async execute(loginData: LoginDto): Promise<string> {
-    console.log('useCase => ', loginData)
-    const hash = await this.hashRepository.encrypt(loginData.password)
-    console.log('hash => ', hash)
-    debugger
-    return 'login'
+  async execute(loginData: LoginDto, response: Response): Promise<ModelOutput> {
+    // find user
+    const user = await this.employeeRepository.findByEmail(loginData.email)
+    // compare hash
+
+    if (!user) {
+      return new ModelOutput({
+        hasError: true,
+        data: null,
+        error: 'email or password is invalid'
+      })
+    }
+
+    // happy path
+
+    const isPasswordValid = await this.hashRepository.compare(
+      loginData.password,
+      user.initialPassword ? user.initialPassword : user.password
+    )
+
+    if (!isPasswordValid) {
+      return new ModelOutput({
+        hasError: true,
+        data: null,
+        error: 'email or password is invalid'
+      })
+    }
+
+    const payload = { email: loginData.email, userId: user.id }
+
+    const token = await this.jwtService.signAsync(payload)
+    response.cookie('Authorization', token, {
+      secure: true,
+      httpOnly: true
+    })
+
+    return new ModelOutput({
+      hasError: false,
+      data: token,
+      error: null
+    })
   }
 }
--- src/modules/session/domain/repositories/encrypt.port.ts
+++
@@ -1,4 +0,0 @@
-export interface IEncryptPort {
-  encrypt(value: string): Promise<string>
-  compare(value: string, hash: string): Promise<boolean>
-}
