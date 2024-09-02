46277d9a737a732cd90abfb5fdee08502a1abc93
Robson Maia <robsonmvieira@gmail.com>
2024-09-02 20:30:27 +0000

    feat(business): :sparkles: register product dependency injection

--- src/modules/logistic/modules/product/product.module.ts
+++ src/modules/logistic/modules/product/product.module.ts
@@ -10,8 +10,11 @@ import {
 } from './application/use-cases'
 import { DatabaseModule } from '@modules/database/database.module'
 import { DataSource } from 'typeorm'
-import { CategoryTypeORMRepository } from './repositories/category.typeorm.repository'
-import { CategoryModel } from './domain/models'
+import { CategoryModel, ProductModel } from './domain/models'
+import {
+  ProductTypeORMRepository,
+  CategoryTypeORMRepository
+} from './repositories'
 
 @Module({
   imports: [DatabaseModule],
@@ -24,6 +27,12 @@ import { CategoryModel } from './domain/models'
       provide: 'ICategoryRepository',
       useFactory: (data: DataSource) =>
         new CategoryTypeORMRepository(data.getRepository(CategoryModel)),
+      inject: ['dbConnectionTypeOrm']
+    },
+    {
+      provide: 'IProductRepository',
+      useFactory: (data: DataSource) =>
+        new ProductTypeORMRepository(data.getRepository(ProductModel)),
       inject: ['dbConnectionTypeOrm']
     }
   ]
