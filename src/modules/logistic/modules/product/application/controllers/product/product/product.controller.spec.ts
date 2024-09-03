009d0a45325c81360a7107119211c07fae76b741
Robson Maia <robsonmvieira@gmail.com>
2024-09-03 21:48:34 +0000

    test(tests): :white_check_mark: fix standard tests

--- src/modules/logistic/modules/product/application/controllers/product/product/product.controller.spec.ts
+++ src/modules/logistic/modules/product/application/controllers/product/product/product.controller.spec.ts
@@ -1,18 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ProductController } from './product.controller';
+import { Test, TestingModule } from '@nestjs/testing'
+import { ProductController } from './product.controller'
+import { CreateProductUseCase } from '../../../use-cases'
 
 describe('ProductController', () => {
-  let controller: ProductController;
+  let controller: ProductController
+  let createProductUseCase: CreateProductUseCase
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
-    }).compile();
+      providers: [
+        {
+          provide: CreateProductUseCase,
+          useValue: {
+            execute: jest.fn()
+          }
+        }
+      ]
+    }).compile()
 
-    controller = module.get<ProductController>(ProductController);
-  });
+    controller = module.get<ProductController>(ProductController)
+    createProductUseCase =
+      module.get<CreateProductUseCase>(CreateProductUseCase)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+    expect(createProductUseCase).toBeDefined()
+  })
+})
--- src/modules/logistic/modules/product/application/use-cases/product/create-product-use-case/create-product-use-case-int.spec.ts
+++ src/modules/logistic/modules/product/application/use-cases/product/create-product-use-case/create-product-use-case-int.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { CreateProductUseCase } from './create-product-use-case'
+import { IProductRepository } from '@modules/logistic/modules/product/domain/repositories'
 
 describe('CreateProductUseCase', () => {
   let service: CreateProductUseCase
+  let repo: IProductRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateProductUseCase]
+      providers: [
+        CreateProductUseCase,
+        {
+          provide: 'IProductRepository',
+          useValue: {
+            save: jest.fn()
+          }
+        }
+      ]
     }).compile()
 
     service = module.get<CreateProductUseCase>(CreateProductUseCase)
+    repo = module.get<IProductRepository>('IProductRepository')
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+    expect(repo).toBeDefined()
   })
 })
