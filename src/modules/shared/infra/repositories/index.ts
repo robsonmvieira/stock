dd43b6542eb2a8b1b534544a1ea53d8520a87edd
Robson Maia <robsonmvieira@gmail.com>
2024-09-07 18:37:49 +0000

    feat(infra): :sparkles: create concret class to implement message broker contract

--- src/modules/shared/infra/repositories/index.ts
+++ src/modules/shared/infra/repositories/index.ts
@@ -1,1 +1,3 @@
 export * from './unit-of-work.typeorm'
+export * from './rabbitmq.message-broker'
+export * from './events-config'
---
+++ src/modules/shared/infra/repositories/rabbitmq.message-broker.ts
@@ -0,0 +1,13 @@
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq'
+import { IMessageBroker } from '@modules/core/application/messages'
+import { IDomainEvent } from '@modules/core/domain/entities'
+import { EVENTS_CONFIG } from './'
+
+export class RabbitMQMessageBroker implements IMessageBroker {
+  constructor(private coon: AmqpConnection) {}
+  async publishEvent(message: IDomainEvent): Promise<void> {
+    const { exchange, routingKey } = EVENTS_CONFIG[message.constructor.name]
+
+    await this.coon.publish(exchange, routingKey, message)
+  }
+}
